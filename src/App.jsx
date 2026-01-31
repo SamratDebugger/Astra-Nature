70c99800a2d4221840a90cb7a444996cb1600831
Samrat <md721023@gmail.com>
2026-01-31 22:55:00 +0600

    services section corrected

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,8 @@ import Avatar from "./Avatar";
 import About from "./components/About";
 import BackgroundImg from "./components/BackgroundImg";
 import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
 import Question from "./components/Question";
-import Services from "./components/services";
+import Services from "./components/Services";
 
 function App() {
   return (

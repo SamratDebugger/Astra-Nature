1c19003c710ec14603198e717f477d4dfe71aa50
Samrat <md721023@gmail.com>
2026-02-03 19:01:22 +0600

    json card  section done

--- src/components/services/Card.jsx
+++ src/components/services/Card.jsx
@@ -1,23 +1,16 @@
-export default function Card() {
+export default function Card({ service }) {
   return (
     <div className="max-w-360 mx-auto  flex flex-col md:flex-row gap-10 lg:gap-20 items-center ">
       <img
-        src="/assets/img/service-1.jpg"
-        alt="service-1 img"
+        src={service.img}
+        alt={service.title}
         className="w-full md:max-w-md"
       />
       <div className="flex flex-col gap-10 items-start">
-        <h2 className="text-5xl font-forum text-main">Web Design</h2>
-        <p className=" text-lg text-main">
-          Tell your visitor how your service can improve their life. Connect
-          with the problem that they&apos;re trying to solve and address any
-          objections you think they might have.
-        </p>
-        <p className="text-main text-lg">
-          What is the biggest benefit your customer will get from this? Keep
-          your target audience in mind.
-        </p>
-        <h3 className="text-4xl text-main">From $99</h3>
+        <h2 className="text-5xl font-forum text-main">{service.title}</h2>
+        <p className="text-lg text-main">{service.description}</p>
+        <p className="text-lg text-main">{service.shortDescription}</p>
+        <h3 className="text-4xl text-main">From ${service.price}</h3>
         <button className="btn btn-lg btn-outline text-main border-main ">
           Start a Project
         </button>
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,6 @@
 import BackgroundImg from "../components/BackgroundImg";
 import Card from "../components/services/Card";
+import services from "/json/services.json";
 
 export default function Services() {
   return (
@@ -7,8 +8,10 @@ export default function Services() {
       <BackgroundImg image="bg-[url(/assets/img/services-hero.jpg)]">
         <h1 className="text-white text-7xl font-medium ">our services</h1>
       </BackgroundImg>
-      <section className="p-5">
-        <Card />
+      <section className="p-5 flex flex-col gap-10">
+        {services.map((service) => (
+          <Card key={service.id} service={service} />
+        ))}
       </section>
     </main>
   );
